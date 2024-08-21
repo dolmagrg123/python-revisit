b7b2f22423961af76496d6306f85972956acc55f
Dolma <dolmatgurung1@gmail.com>
2024-08-21 22:50:37 +0000

    python data types and type conversions

---
+++ info.py
@@ -0,0 +1,11 @@
+name = "Wanda"
+print(name)
+
+second_name = "Bruce"
+
+print(name + " " + second_name)
+weight_string = "300"
+print ( "My name is " + second_name + " and my weight is " + weight_string)
+
+height = "6ft3in"
+print(height)
---
+++ information.py
@@ -0,0 +1,20 @@
+name = "Wanda"
+print(name)
+
+second_name = "Bruce"
+
+print(name + " " +  second_name)
+print(name , second_name)
+
+weight = 300
+print(weight)
+# if we combine strings, we can use + to add strings
+# but use comma(,) to add strings with integers
+
+print( "My name is " , second_name, "and my weight is", weight)
+
+weight_string = "300"
+print ( "My name is " + second_name + " and my weight is " + weight_string)
+
+height = "6'3"
+print(height)
