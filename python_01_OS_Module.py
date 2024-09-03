dd0c7c100888e7a891ae5f4ec022b46e4447bf0d
Ubuntu <ubuntu@ip-172-31-51-150.ec2.internal>
2024-09-03 22:25:16 +0000

    use of function

--- python_01_OS_Module.py
+++ python_01_OS_Module.py
@@ -1,17 +1,22 @@
 import os
 
-dir = input("Enter the path of directory do you want to count the number of items?")
+def count_items():
+    dir = input("Enter the path of directory do you want to count the number of items?")
 
-no_of_file = 0
-no_of_dir = 0
+    no_of_file = 0
+    no_of_dir = 0
 
-directory = os.listdir(dir)
-# print(directory)
+    # use os.listdir(dir) function to return list of files and directory present
+    directory = os.listdir(dir)
 
-for item in directory:
-    if os.path.isfile(os.path.join(dir, item)) :
-        no_of_file +=1
-    else:
-        no_of_dir +=1
-print("Number of file = ", no_of_file)
-print("Number of dir = ", no_of_dir)
+    for item in directory:
+        # used os.path.join to add the item in the complete path
+        # os.path.isfile to check if an item is file or directory
+        if os.path.isfile(os.path.join(dir, item)) :
+            no_of_file +=1
+        else:
+            no_of_dir +=1
+    print("Number of file = ", no_of_file)
+    print("Number of dir = ", no_of_dir)
+
+count_items()
