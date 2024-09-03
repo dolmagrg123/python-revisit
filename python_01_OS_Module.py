026351e4fbc6d52be4a4ad4eba1335930cdf85a1
Ubuntu <ubuntu@ip-172-31-51-150.ec2.internal>
2024-09-03 22:52:56 +0000

    error handling

--- python_01_OS_Module.py
+++ python_01_OS_Module.py
@@ -5,18 +5,22 @@ def count_items():
 
     no_of_file = 0
     no_of_dir = 0
+    try:
+        # use os.listdir(dir) function to return list of files and directory present
+        directory = os.listdir(dir)
 
-    # use os.listdir(dir) function to return list of files and directory present
-    directory = os.listdir(dir)
-
-    for item in directory:
-        # used os.path.join to add the item in the complete path
-        # os.path.isfile to check if an item is file or directory
-        if os.path.isfile(os.path.join(dir, item)) :
-            no_of_file +=1
-        else:
-            no_of_dir +=1
-    print("Number of file = ", no_of_file)
-    print("Number of dir = ", no_of_dir)
+        for item in directory:
+            # used os.path.join to add the item in the complete path
+            # os.path.isfile to check if an item is file or directory
+            if os.path.isfile(os.path.join(dir, item)) :
+                no_of_file +=1
+            else:
+                no_of_dir +=1
+        print("Number of file = ", no_of_file)
+        print("Number of dir = ", no_of_dir)
+    except FileNotFoundError:
+        print(f"{dir} does not exist")
+        count_items()
+    
 
 count_items()
