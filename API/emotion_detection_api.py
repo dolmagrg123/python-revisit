262e090db6a8ae282430a64984dd7c8deacd960c
Ubuntu <ubuntu@ip-172-31-51-150.ec2.internal>
2024-09-05 00:53:17 +0000

    error handling

--- API/emotion_detection_api.py
+++ API/emotion_detection_api.py
@@ -14,11 +14,12 @@ url = "https://emotion-detection2.p.rapidapi.com/emotion-detection"
 
 payload1 = { "url": "https://as2.ftcdn.net/v2/jpg/00/56/93/53/1000_F_56935312_NiqxkRKOdGSJd86Tc2uLycL9fkUsIlRW.jpg" }
 
-# payload2 = { "url": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" }
+payload2 = { "url": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" }
 
-# payload2 = {"url": "https://live-production.wcms.abc-cdn.net.au/4cc0c19aad68a86fac83bf86dcba4a6f?impolicy=wcms_crop_resize&cropH=2813&cropW=5000&xPos=0&yPos=353&width=862&height=485"}
+payload3 = {"url": "https://live-production.wcms.abc-cdn.net.au/4cc0c19aad68a86fac83bf86dcba4a6f?impolicy=wcms_crop_resize&cropH=2813&cropW=5000&xPos=0&yPos=353&width=862&height=485"}
 
-payload2 = {"url": "https://live-production.wcm.net"}
+#giving incorrect url to test
+# payload2 = {"url": "https://live-production.wcm.net"}
 
 
 def emotion_detection(payload):
@@ -32,10 +33,8 @@ def emotion_detection(payload):
         var = response.json()
 
         for item in var:
-            # print(item)
             if item["probability"] >= 0.99:
                 print(f"The emotion presented in the picture is {item["emotion"]["value"]}")
-                # return item["emotion"]["value"]
             else:
                 print("Probability is less than 0.99, Cannot detect Correct emotion")
     except:
