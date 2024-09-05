4839e32e24a5de9fc39baa80f6ed362be76ea1ea
Ubuntu <ubuntu@ip-172-31-51-150.ec2.internal>
2024-09-05 23:07:20 +0000

    modifying api data

--- mock_interview/hello.py
+++ mock_interview/hello.py
@@ -1,11 +1,26 @@
 import requests
+from pprint import pprint
+
 url = "https://api.travelpayouts.com/v2/prices/month-matrix"
+
 querystring = {"currency":"usd","show_to_affiliates":"true","origin":"NYC","destination":"LAX"}
+
 headers = {'x-access-token': '2160a9f9ca2fa3d348f4a3a32504538e'}
+
 response = requests.request("GET", url, headers=headers, params=querystring)
-a = response.json()
-print(a)
 
-a.keys()
 
-print(a["data"]["depart_date"])
+a = (response.json())
+
+onelayer = a['data']
+
+for item in onelayer:
+    if item["depart_date"] == "2024-10-17":
+        print(item["value"])
+    
+
+# print(onelayer[0])
+
+# print(a.keys())
+
+# pprint(response.json(), compact = True)
