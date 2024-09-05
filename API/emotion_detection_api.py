55af0bef50942e91d180265ae5b5fd3cfbd7e4ef
Ubuntu <ubuntu@ip-172-31-51-150.ec2.internal>
2024-09-05 00:43:04 +0000

    Added function to process multiple images

--- API/emotion_detection_api.py
+++ API/emotion_detection_api.py
@@ -14,8 +14,9 @@ url = "https://emotion-detection2.p.rapidapi.com/emotion-detection"
 
 payload1 = { "url": "https://as2.ftcdn.net/v2/jpg/00/56/93/53/1000_F_56935312_NiqxkRKOdGSJd86Tc2uLycL9fkUsIlRW.jpg" }
 
-payload2 = { "url": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" }
+# payload2 = { "url": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" }
 
+payload2 = {"url": "https://live-production.wcms.abc-cdn.net.au/4cc0c19aad68a86fac83bf86dcba4a6f?impolicy=wcms_crop_resize&cropH=2813&cropW=5000&xPos=0&yPos=353&width=862&height=485"}
 
 def emotion_detection(payload):
     headers = {
@@ -27,8 +28,15 @@ def emotion_detection(payload):
     response = requests.post(url, json=payload, headers=headers)
     var = response.json()
 
-    print(f"The emotion presented in the picture is {var[0]["emotion"]["value"]}")
+    for item in var:
+        # print(item)
+        if item["probability"] >= 0.99:
+            # print(f"The emotion presented in the picture is {item["emotion"]["value"]}")
+            return item["emotion"]["value"]
+        else:
+            print("Probability is less than 0.99, Cannot detect Correct emotion")
+            return None
 
-emotion_detection(payload1)
-emotion_detection(payload2)
+print("The first image emotion is", emotion_detection(payload1))
+print("the second image emotion is", emotion_detection(payload2))
 
