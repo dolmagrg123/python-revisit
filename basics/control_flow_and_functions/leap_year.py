6912785ea9d17f51152022723d5e46192c4be85a
Dolma <dolmatgurung1@gmail.com>
2025-02-12 23:41:29 -0500

    leap_year conditionals

---
+++ basics/control_flow_and_functions/leap_year.py
@@ -0,0 +1,20 @@
+
+
+def even_odd(user_input):
+    if user_input%2 == 0:
+        print ("even")
+    else:
+        print("odd")
+
+def leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print("leap year")
+    else:
+        print("not a leap year")
+
+
+user_input = int(input("Enter a number"))
+even_odd(user_input)
+
+year = int(input("Enter a year"))
+leap_year(year)
